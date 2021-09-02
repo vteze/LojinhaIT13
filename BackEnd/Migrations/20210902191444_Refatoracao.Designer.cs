94e220db0c2eb4043cc63d08b9b199574cc2d396
Marco Chitolina <marcoantonio.c.s.2000@gmail.com>
2021-09-02 16:15:43 -0300

    nova migração

--- BackEnd/Migrations/20210902191444_Refatoracao.Designer.cs
+++ BackEnd/Migrations/20210902191444_Refatoracao.Designer.cs
@@ -4,14 +4,16 @@ using LojinhaIT13.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LojinhaIT13.Migrations
 {
     [DbContext(typeof(BdContext))]
-    partial class BdContextModelSnapshot : ModelSnapshot
+    [Migration("20210902191444_Refatoracao")]
+    partial class Refatoracao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BackEnd/Migrations/20210902191444_Refatoracao.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojinhaIT13.Migrations
+{
+    public partial class Refatoracao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarrinhoProduto");
+
+            migrationBuilder.DropTable(
+                name: "Carrinhos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UrlImagem",
+                table: "Produtos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataEmissao",
+                table: "Pedidos",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "UrlImagem",
+                table: "Produtos");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataEmissao",
+                table: "Pedidos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Carrinhos",
+                columns: table => new
+                {
+                    CarrinhoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrinhos", x => x.CarrinhoId);
+                    table.ForeignKey(
+                        name: "FK_Carrinhos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "ClienteId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarrinhoProduto",
+                columns: table => new
+                {
+                    CarrinhoId = table.Column<int>(type: "int", nullable: false),
+                    ProdutoId = table.Column<int>(type: "int", nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarrinhoProduto", x => new { x.CarrinhoId, x.ProdutoId });
+                    table.ForeignKey(
+                        name: "FK_CarrinhoProduto_Carrinhos_CarrinhoId",
+                        column: x => x.CarrinhoId,
+                        principalTable: "Carrinhos",
+                        principalColumn: "CarrinhoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoProduto_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "ProdutoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoProduto_ProdutoId",
+                table: "CarrinhoProduto",
+                column: "ProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carrinhos_ClienteId",
+                table: "Carrinhos",
+                column: "ClienteId",
+                unique: true);
+        }
+    }
+}

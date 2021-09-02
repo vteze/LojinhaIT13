7fcbd09e987efba235a85d759a1073fe6d9115ad
Rafael Coll <Rafael_Oliveira3@Dellteam.com>
2021-09-02 18:41:26 -0300

    Adicionado endpoint de buscar Clientes

---
+++ BackEnd/Controllers/ClientesControler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LojinhaIT13.Models;
+using LojinhaIT13.Dtos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
+
+namespace LojinhaIT13.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [EnableCors("AllowAll")]
+    public class ClientesController : ControllerBase
+    {
+        private readonly ILogger<ProdutosController> _logger;
+        private readonly BdContext _basedados;
+
+        public ClientesController(ILogger<ProdutosController> logger, BdContext basedados)
+        {
+            _logger = logger;
+            _basedados = basedados;
+        }
+
+        //GET /clientes
+        [HttpGet]
+        public IEnumerable<ClienteDTO> BuscarTodosClientes([FromQuery] string pesquisa)
+        {
+            if (pesquisa != null)
+            {
+                return _basedados.Clientes
+                    .Select(ClienteDTO.FromCliente)
+                    .Where(cliente => cliente.Nome.Contains(pesquisa.ToLower()));    
+            }
+
+            return _basedados.Clientes.Select(ClienteDTO.FromCliente);
+        }
+    }
+}
